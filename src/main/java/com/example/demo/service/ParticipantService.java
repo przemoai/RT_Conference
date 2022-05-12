7290ea10567e74d310de002bb549c8c9c4f6d2ce
przemoai <again364@gmail.com>
2022-05-12 12:48:52 +0200

    fixed adding and editing participants

--- src/main/java/com/example/demo/service/ParticipantService.java
+++ src/main/java/com/example/demo/service/ParticipantService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Participant;
 import com.example.demo.repository.ParticipantRepository;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,7 @@ public class ParticipantService {
     public ResponseEntity addParticipant(@RequestBody Participant participant) {
 
 
-        if (isParticipantExist(participant)) {
+        if (!isParticipantExist(participant)) {
             try {
                 Participant newParticipant = participantRepository
                         .save(new Participant(participant.getLogin(), participant.getEmail()));
@@ -37,22 +38,28 @@ public class ParticipantService {
         }
     }
 
-    private boolean isParticipantExist(Participant participant) {
-        boolean exists = participantRepository.findByLogin(participant.getLogin()).isEmpty();
+    private boolean isParticipantExist(@NotNull Participant participant) {
+        boolean exists = !participantRepository.findByLogin(participant.getLogin()).isEmpty();
         return exists;
     }
 
     public ResponseEntity updateParticipantEmail(@RequestBody Participant participant) {
-        Optional<Participant> ParticipantData = participantRepository.findById(participant.getId());
 
-        if (ParticipantData.isPresent()) {
-            Participant newParticipant = ParticipantData.get();
-            newParticipant.setEmail(participant.getEmail());
-            return new ResponseEntity<>(participantRepository.save(newParticipant), HttpStatus.OK);
+        if (isParticipantExist(participant)) {
+            Participant updatedParticipant = getParticipant(participant);
+            return new ResponseEntity<>(participantRepository.save(updatedParticipant), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
         }
+    }
+
+    @NotNull
+    private Participant getParticipant(Participant participant) {
+        Optional<Participant> participantData = participantRepository.findById(participant.getId());
+        Participant updatedParticipant=participantData.get();
+        updatedParticipant.setEmail(participant.getEmail());
+        return updatedParticipant;
     }
 
 }
